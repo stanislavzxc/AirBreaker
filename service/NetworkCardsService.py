d57fcee20d2fe3b5d2604351ef7f78da29a7c37d
Stanislavzxc <stanislavborisov67@gmail.com>
2026-08-02 22:46:19 +0700

    fix the netrworkCardSerive

--- service/NetworkCardsService.py
+++ service/NetworkCardsService.py
@@ -1,43 +1,28 @@
 import asyncio
-import re
-import shutil
+import os
 
 async def GetWifiChipsets():
-    if not shutil.which("airmon-ng"):
-        return {"error": "airmon-ng is not installed"}
-    try:
-        process = await asyncio.create_subprocess_exec(
-            "sudo", "airmon-ng",
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
+    interfaces = []
+    main_dir = "/sys/class/net/"
 
-        stdout, stderr = await process.communicate()
-        
-        if process.returncode != 0:
-            error_msg = stderr.decode().strip() or stdout.decode().strip() or "Unknown error"
-            return {"success": False, "error": f"OS error: {error_msg}"}
-
-        lines = stdout.decode('utf-8', errors='ignore').strip().split('\n')
-        interfaces = []
-        start_working = False
-
-        for line in lines:
-            if not line.strip(): 
-                continue
-            if "interface" in line.lower() and "chipset" in line.lower():
-                start_working = True
-                continue
-            if start_working:
-                parts = re.split(r'\t+|\s{2,}', line.strip())
-                if len(parts) >= 4:
-                    interfaces.append({
-                        "phy": parts[0],
-                        "interface": parts[1],
-                        "driver": parts[2],
-                        "chipset": parts[3]
-                    })
-        return interfaces 
+    if not os.path.exists(main_dir):
+        return {"succes": False, "message": "sys/class/net dir doesnt exist, maybe not a linux?" }
     
-    except Exception as e:
-        return {"success": False, "error": str(e)}
+    for iface in os.listdir(main_dir):
+        wifi_dir = os.path.join(main_dir, iface, "wireless")
+        phy_symlink = os.path.join(main_dir, iface, "phy80211")
+        
+        if os.path.exists(wifi_dir) or os.path.exists(phy_symlink):
+            try:
+                with open(os.path.join(main_dir, iface, "operstate"), "r") as f:
+                    state = f.read().strip()
+            except:
+                state = "unknown"
+
+            interfaces.append({
+                "interface": iface,
+                "state": state,
+                "driver": os.path.basename(os.readlink(os.path.join(main_dir, iface, "device", "driver"))) if os.path.exists(os.path.join(main_dir, iface, "device", "driver")) else "unknown"
+            })
+            
+    return interfaces
