6d5cb4f2ebfa516cb30e8a8ee2ae85af3d411b25
Stanislavzxc <stanislavborisov67@gmail.com>
2026-08-05 00:00:03 +0700

    add monitor mode router

--- routers/monitor_mode.py
+++ routers/monitor_mode.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from service.monitor_mode_service import set_monitor_mode_service
-from schemas.monitor import MonitorModeRequest
+from schemas.monitor import MonitorModeRequest,MonitorModeResponse
 
-monitor_router = APIRouter(prefix="/api")
+monitor_router = APIRouter(prefix="/api", tags=["Wi-Fi Monitor Mode"])
 
-@monitor_router.post("/monitor_mode")
-async def set_monitor_mode(device: MonitorModeRequest):
-    result = await set_monitor_mode_service(device)
-    return {result}
+@monitor_router.post("/monitor_mode", response_model = MonitorModeResponse)
+async def set_monitor_mode(request_data: MonitorModeRequest):
+    result = await set_monitor_mode_service(request_data.device)
+    return result
