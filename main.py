8266c9f03c120f4952a7f227804ee24e960e9125
Stanislavzxc <stanislavborisov67@gmail.com>
2026-08-09 22:50:47 +0700

    add swagger black theme

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
 
 from routers import monitor_router, network_card_router, scanning_router
 from service.monitor_mode_service import set_monitor_mode_service
@@ -25,11 +27,25 @@ async def lifespan(app: FastAPI):
 
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    lifespan=lifespan,
+    docs_url=None
+)
 
 app.include_router(network_card_router)
 app.include_router(monitor_router)
 app.include_router(scanning_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get('/')
 def index():
     return "check /docs"
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html_github():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=f"{app.title} - Swagger UI",
+        swagger_css_url="/static/swagger_ui_dark.css"
+    )
