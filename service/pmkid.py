b2794046452903a5dbc21659548b9e840d904634
Stanislavzxc <stanislavborisov67@gmail.com>
2026-09-01 21:46:50 +0700

    add a little logic to pmkid attack

--- service/pmkid.py
+++ service/pmkid.py
@@ -1,7 +1,39 @@
+import asyncio
+
+from scapy.all import AsyncSniffer
 from scapy.layers.dot11 import Dot11, Dot11Auth
 
 from state import app_state
+from utils.network import wifi_packets_callback, wifi_packets_clear
 
+#current target. mass-attack will be add in future, maybe
 
 class PmkidService():
-    pass
+    def __init__(self):
+        self._sniffer : AsyncSniffer | None = None
+        self.queue : asyncio.Queue = asyncio.Queue()
+        self.auth_task : asyncio.Task | None = None
+
+    async def start_capture(self, device):
+        if self._sniffer and self._sniffer.running:
+            return
+        self.queue = asyncio.Queue
+        wifi_packets_clear()
+
+        loop = asyncio.get_running_loop()
+        self._sniffer = AsyncSniffer(
+            iface=device,
+            prn=lambda pkt : wifi_packets_callback(pkt, self.queue, loop),
+            store=0
+        )
+        self._sniffer.start()
+        print("pmkid sniffer was started")
+
+        await asyncio.sleep(1)
+
+        
+    async def stop_capture():
+        pass
+
+    async def stream_results():
+        pass
