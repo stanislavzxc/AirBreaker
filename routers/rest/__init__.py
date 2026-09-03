37cd207b6d15a569011aec07f4b6be56fe693ce7
Stanislavzxc <stanislavborisov67@gmail.com>
2026-09-03 22:56:09 +0700

    add routers/rest

---
+++ routers/rest/__init__.py
@@ -0,0 +1,3 @@
+from .choose_network import current_network_router
+from .monitor_mode import monitor_router
+from .network_cards import network_card_router
--- utils/network/__init__.py
+++ utils/network/__init__.py
@@ -1,9 +1,9 @@
 from .channel_hopper import channel_hopper, get_available_channels
-from .deauth_packets import PacketsBuilder
 from .generate_mac import generate_random_mac
 from .get_bssid import get_bssid
 from .network_card.check_network_card import check_network_card
 from .network_card.check_network_card_mode import check_network_card_mode
 from .network_card.network_cards import get_wifi_chipsets
 from .network_services import network_services_awake, network_services_kill
+from .packets_builder import PacketsBuilder
 from .wifi_core import wifi_packets_callback, wifi_packets_clear
