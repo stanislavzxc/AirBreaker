fd62aec4a6e2edc8edfa88202d43746d815626e6
Stanislavzxc <stanislavborisov67@gmail.com>
2026-09-19 17:15:48 +0700

    add tests for check_depends

---
+++ tests/utils/system/test_check_depends.py
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import pytest
+
+from utils.system import check_depends
+
+
+async def test_all_deps_present(monkeypatch):
+    monkeypatch.setattr("shutil.which", lambda name: f"/usr/bin/{name}")
+
+    wanted = ["ip", "iw", "iwlist"]
+    missing = await check_depends(wanted)
+
+    assert missing == []
+
+async def test_nothing_deps_present(monkeypatch):
+    monkeypatch.setattr("shutil.which", lambda name: None)
+
+    wanted = ["ip", "iw", "iwlist"]
+    missing = await check_depends(wanted)
+
+    assert missing == wanted
+
+async def test_patrial_deps_present(monkeypatch):
+    wanted = ["ip", "iw", "iwlist"]
+    installed = ["ip"]
+
+    monkeypatch.setattr("shutil.which", lambda name: f"/usr/bin/{name}" if name in installed else None)
+
+    missing = await check_depends(wanted)
+
+    assert missing == ["iw", "iwlist"]
+    
+async def test_is_duplicat(monkeypatch):
+    wanted = ["ip", "ip", "iwlist"]
+    monkeypatch.setattr("shutil.which", lambda name: None)
+
+    missing = await check_depends(wanted)
+
+    assert missing == ["ip", "iwlist"]
