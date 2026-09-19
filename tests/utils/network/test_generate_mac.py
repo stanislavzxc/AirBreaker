111820bde62ba9145ae5c943a3e20e1f7c052e9f
Stanislavzxc <stanislavborisov67@gmail.com>
2026-09-19 23:48:39 +0700

    fix the test

--- tests/utils/network/test_generate_mac.py
+++ tests/utils/network/test_generate_mac.py
@@ -4,11 +4,11 @@ import re
 
 import pytest
 
-from utils.network import generate_mac
+from utils.network import generate_random_mac
 
 
-def dummy_test():
-    mac : str = generate_mac()
+def test_is_work():
+    mac : str = generate_random_mac()
 
     assert len(mac) == 17
     assert re.match(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", mac) is not None
